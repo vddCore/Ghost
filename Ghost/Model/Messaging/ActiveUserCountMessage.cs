f22bb518aa4f5171af84258e0104f7acff90754c
Ciastex <Ciastex@users.noreply.github.com>
2020-06-05 00:11:25 +0200

    usability improvements

---
+++ Ghost/Model/Messaging/ActiveUserCountMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Ghost.Model.Messaging
+{
+    internal class ActiveUserCountMessage
+    {
+        public int CurrentCount { get; }
+
+        public ActiveUserCountMessage(int currentCount)
+            => CurrentCount = currentCount;
+    }
+}
---
+++ Ghost/Model/Messaging/ChatStatusMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Ghost.Model.Messaging
+{
+    internal class ChatStatusMessage
+    {
+        public string Status { get; set; }
+
+        public ChatStatusMessage(string status)
+            => Status = status;
+    }
+}
