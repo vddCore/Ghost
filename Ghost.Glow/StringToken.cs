64e83239d10af5644fd70d99b3e3ceeb4e0827a2
Ciastex <mrciastexx@gmail.com>
2020-06-07 16:41:41 +0200

    improve deobfuscator

---
+++ Ghost.Glow/StringToken.cs
@@ -0,0 +1,9 @@
+namespace Ghost.Glow
+{
+    public class StringToken
+    {
+        public string Value { get; set; }
+        public string DecryptedValue { get; set; }
+        public char DelimiterChar { get; set; }
+    }
+}
